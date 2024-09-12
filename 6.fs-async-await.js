6b1de4322b57d688e428100b0a334c16c6acb811
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-11 22:19:33 -0600

    funciones IIFE
    Manejo de funciones IIFE Inmediately Invoked Function Expression

---
+++ 6.fs-async-await.js
@@ -0,0 +1,33 @@
+const { readFile } = require('node:fs/promises');
+
+
+async function init(){
+    console.log('Leyendo primer archivo...');
+    const text = await readFile('archivo.txt', 'utf-8');
+    console.log(text);
+    console.log('Hacer algo mientras se termina de leer...');
+    
+    console.log('Leyendo segundo archivo...');
+    const text2 = await readFile('archivo2.txt', 'utf-8');
+    console.log(text2);
+}
+
+init();
+
+
+// IIFE (Immediately Invoked Function Expression)
+/*
+(
+    async () => {
+
+        console.log('Leyendo primer archivo...');
+        const text = await readFile('archivo.txt', 'utf-8');
+        console.log(text);
+        console.log('Hacer algo mientras se termina de leer...');
+        
+        console.log('Leyendo segundo archivo...');
+        const text2 = await readFile('archivo2.txt', 'utf-8');
+        console.log(text2);
+    }
+)();
+*/
