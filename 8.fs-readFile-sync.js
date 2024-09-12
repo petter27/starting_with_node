295346e919e02a9b2f4c2ce702a6e831ae8f200b
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-11 22:26:15 -0600

    Leer archivos de forma sincrona

---
+++ 8.fs-readFile-sync.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');
+
+console.log('Leyendo primer archivo...');
+const texto= fs.readFileSync('archivo.txt', 'utf-8');
+console.log(texto);
+
+console.log('Hacer algo mientras se termina de leer...');
+
+console.log('Leyendo segundo archivo...');
+const text2 = fs.readFileSync('archivo2.txt', 'utf-8');
+
+console.log(text2);
