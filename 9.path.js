9b47c243982b0c218d7de05974da528bb3740a87
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-11 22:41:28 -0600

    Modulo Path

---
+++ 9.path.js
@@ -0,0 +1,19 @@
+const path = require('node:path');
+
+let separador = path.sep;
+console.log("seperador: ", separador);
+
+let ruta = path.join(__dirname, 'archivo.txt');
+console.log("ruta del archivo:",ruta);
+
+// devuelve el nombre del archivo
+const base = path.basename(ruta);
+console.log("nombre del archivo:",base);
+
+// nombre del archivo sin la extensión
+const nombre = path.basename(ruta, '.txt');
+console.log("nombre del archivo sin extensión:",nombre);
+
+// devuelve la extensión del archivo
+const ext = path.extname(ruta);
+console.log("extensión del archivo:",ext);
