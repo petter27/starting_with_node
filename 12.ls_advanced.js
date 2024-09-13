0cf9e5950640cd52fd98880244e5acabc1a5bb47
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-12 23:09:03 -0600

    file system avanzado

---
+++ 12.ls_advanced.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+// Podemos pasar el directorio como argumento
+const folder = process.argv[2] ?? '.'; 
+
+async function listFiles(directory) {
+    let files;
+    try{
+         files = await fs.readdir(directory);
+    }catch(err){
+        console.error(`No se pudo leer el directorio: ${directory}`);
+        process.exit(1);
+    }
+
+    const filesPromises = files.map(async (file) => {
+        const filePath = path.join(directory, file);
+
+        let stats;
+        try{
+            stats = await fs.stat(filePath);
+        }catch(err){
+            console.error(`No se pudo leer el archivo: ${filePath}`);
+            process.exit(1);
+        }
+
+        const isDirectory = stats.isDirectory();
+        const fileType = isDirectory ? 'DIR' : 'FILE';
+        const fileSize = stats.size.toString();
+        const fileModified = stats.mtime.toLocaleString();
+    
+        return `${fileType.padEnd(5)} | ${file.padEnd(30)} | ${fileSize.toString().padStart(10)} | ${fileModified}`;
+
+    })
+
+    const filesInfo = await Promise.all(filesPromises)
+    filesInfo.forEach((file) => {
+        console.log(file);
+    });
+}
+
+listFiles(folder);
