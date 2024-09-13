7c33a40f7437ae97d922571ec5d4b1d44dc60630
Pedro Osvaldo Pacheco Martinez <pedro.pacheco@catolica.edu.sv>
2024-09-13 00:05:00 -0600

    Update 6.fs-async-await.js

--- 6.fs-async-await.js
+++ 6.fs-async-await.js
@@ -1,20 +1,18 @@
 // asincrono secuencial con async-await
 const { readFile } = require('node:fs/promises');
 
+async function init () {
+  console.log('Leyendo primer archivo...');
+  const text = await readFile('archivo.txt', 'utf-8');
+  console.log(text);
+  console.log('Hacer algo mientras se termina de leer...');
 
-async function init(){
-    console.log('Leyendo primer archivo...');
-    const text = await readFile('archivo.txt', 'utf-8');
-    console.log(text);
-    console.log('Hacer algo mientras se termina de leer...');
-    
-    console.log('Leyendo segundo archivo...');
-    const text2 = await readFile('archivo2.txt', 'utf-8');
-    console.log(text2);
+  console.log('Leyendo segundo archivo...');
+  const text2 = await readFile('archivo2.txt', 'utf-8');
+  console.log(text2);
 }
 
 init();
-
 
 // IIFE (Immediately Invoked Function Expression)
 /*
@@ -25,7 +23,7 @@ init();
         const text = await readFile('archivo.txt', 'utf-8');
         console.log(text);
         console.log('Hacer algo mientras se termina de leer...');
-        
+
         console.log('Leyendo segundo archivo...');
         const text2 = await readFile('archivo2.txt', 'utf-8');
         console.log(text2);
